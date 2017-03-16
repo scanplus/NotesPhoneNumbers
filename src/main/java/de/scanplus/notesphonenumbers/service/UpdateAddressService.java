0045859cd2e2d0b99922aa8e43e9acd7cf1a5e5a
Tobias Genannt <t.genannt@scanplus.de>
2017-03-16 17:38:53 +0100

    Loading max Entries from ENV

--- src/main/java/de/scanplus/notesphonenumbers/service/UpdateAddressService.java
+++ src/main/java/de/scanplus/notesphonenumbers/service/UpdateAddressService.java
@@ -12,18 +12,33 @@ import org.springframework.stereotype.Service;
 @Service
 public class UpdateAddressService {
 
+    private static final String ENV_LOAD_ENTRIES_PER_REQUEST = "LOAD_ENTRIES_PER_REQUEST";
+    
     private static final Logger LOG = LogManager.getLogger(UpdateAddressService.class);
 
     @Autowired
     private DominoSalesClient dsc;
     
+    private int loadEntriesPerRequest = 50;
+
+    public UpdateAddressService() {
+        final String loadEntriesPerRequest = System.getenv(ENV_LOAD_ENTRIES_PER_REQUEST);
+        try {
+            this.loadEntriesPerRequest = Integer.parseInt(loadEntriesPerRequest);
+        } catch (Exception ex) {
+            LOG.warn("Could not load loadEntriesPerRequest from ENV; using default = "
+                    + this.loadEntriesPerRequest);
+        }
+    }
+    
+        
     @Scheduled(fixedDelay = 1000 * 60 * 60, initialDelay = 1000 * 60)
     public void execute() {
         if (!dsc.isReady()) {
             LOG.error("Could not initialise API client");
             return;
         }
-        final List<AddressLink> list = dsc.loadLinkList(0, 6000);
+        final List<AddressLink> list = dsc.loadLinkList(0, loadEntriesPerRequest);
         int count = 1;
         for (final AddressLink al : list) {
             AddressData ad = dsc.loadAddressData(al);
